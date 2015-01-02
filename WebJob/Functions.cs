a8de8a35581e47c14db767ffcc2daa97d49e8237
Sean Feldman <feldman.sean@gmail.com>
2015-01-01 22:09:16 -0700

    basic web app and job

---
+++ WebJob/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJob
+{
+    public class Functions
+    {
+        [NoAutomaticTrigger]
+        public static void Host(TextWriter log, CancellationToken cancellationToken)
+        {
+            var tzi = TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time");
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                log.WriteLine("Host: still running at " + TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzi));
+                Thread.Sleep(3000);
+            }
+
+            log.WriteLine("Host: Canceled at " + TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzi));
+        }
+    }
+}
---
+++ WebJob/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+
+namespace WebJob
+{
+    class Program
+    {
+        static void Main()
+        {
+            var host = new JobHost();
+            host.Call(typeof(Functions).GetMethod("Host"));
+            host.RunAndBlock();
+        }
+    }
+}
